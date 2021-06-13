d02a7a0a4434216b841321cbc2772f675194fbf2
JoaoGabriel27 <83670539+JoaoGabriel27@users.noreply.github.com>
2021-06-13 15:32:52 -0300

    Update Double-Spring.jl

--- Double-Spring.jl
+++ Double-Spring.jl
@@ -15,4 +15,7 @@ l₂ = x₀₂ - x₀₁ - w₁ - r₂ #Strech of the second spring
 y₀ = [x₀₁; x₀₂; v₀₁; v₀₂]#Initial values for position 1, 2 and velocity 1, 2 respectively 
 t0, tf = 0.0, 20         #Initial and final time
 
-t, D = rungekutta4(t0, tf, y₀, F; N=500)
+t, D = rungekutta4(t0, tf, y₀, F) #t is the time vector and D is a matrix with 4 lines and interation + 1 number of columns where line 1 = positions block 1
+                                                                                                                                 #line 2 = positions block 2
+                                                                                                                                 #line 3 = velocities block 1
+                                                                                                                                 #line 4 = velocities block 2        
