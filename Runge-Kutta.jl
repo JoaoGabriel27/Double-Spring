5d779b57ed0b59d5b6d8130507a90e7a5453a8c1
JoaoGabriel27 <83670539+JoaoGabriel27@users.noreply.github.com>
2021-06-16 11:36:09 -0300

    Update Runge-Kutta.jl

--- Runge-Kutta.jl
+++ Runge-Kutta.jl
@@ -1,10 +1,16 @@
-function rungekutta4(t0, tf, y0, f; N=100)
+function rungekutta4col(t0, tf, y0, m₁, m₂, f; N=100)
     t = range(t0, tf, length=N+1)
     h = t[2] - t[1]
     m = length(y0)
     w = zeros(m, N+1)
     w[:,1] = y0
     for k = 1:N
+        if (w[2,k] - w[1,k] < 0) 
+            w[3,k], w[4,k] = ((m₁ - m₂)*w[3,k] + 2*m₂*w[4,k])/(m₁ + m₂), ((m₂ - m₁)*w[4,k] + 2*m₁*w[3,k])/(m₁ + m₂)
+        end
+        if (w[1,k] < 0)
+            w[3,k] = -w[3,k]
+        end
         θ₁ = f(t[k],w[:,k])
         θ₂ = f(t[k] + (h / 2), w[:,k] + (h * (θ₁ / 2)))
         θ₃ = f(t[k] + (h / 2), w[:,k] + (h * (θ₂ / 2)))
