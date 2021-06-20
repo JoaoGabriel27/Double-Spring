95c89e018918d5017279a4e202979ed0e2e88e4b
JoaoGabriel27 <83670539+JoaoGabriel27@users.noreply.github.com>
2021-06-20 14:58:21 -0300

    Update Runge-Kutta-Collisions.jl

--- Runge-Kutta-Collisions.jl
+++ Runge-Kutta-Collisions.jl
@@ -5,11 +5,11 @@ function rungekutta4col(t0, tf, y0, m₁, m₂, f; N=100)
     w = zeros(m, N+1)
     w[:,1] = y0
     for k = 1:N
-        if (w[2,k] - w[1,k] < 0) #In case the two block were in the same place at the same time, in means a collision happened
+        if (w[2,k] - w[1,k] <= 0) #In case the two block were in the same place at the same time, in means a collision happened
             w[3,k], w[4,k] = ((m₁ - m₂)*w[3,k] + 2*m₂*w[4,k])/(m₁ + m₂), ((m₂ - m₁)*w[4,k] + 2*m₁*w[3,k])/(m₁ + m₂) #In this case, we are considering a perfectly elastic collision
         end                                                                                                         #vf₁ = ((m₁ - m₂)*vi₁ + 2*m₂*vi₂)/(m₁ + m₂)
                                                                                                                     #vf₂ = ((m₂ - m₁)*vi₂ + 2*m₂*vi₁)/(m₁ + m₂)
-        if (w[1,k] < 0) #In case the first block and the fixed point were in the same place at the same time, in means a collision happened
+        if (w[1,k] <= 0) #In case the first block and the fixed point were in the same place at the same time, in means a collision happened
             w[3,k] = -w[3,k] #In this case, we are considering a perfectly elastic collision as well
         end                  #vf₁ = -vi₁
         θ₁ = f(t[k],w[:,k])
