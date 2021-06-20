6a99022a2ba07e0b0dbfbe4c7247c7d201a734f7
JoaoGabriel27 <83670539+JoaoGabriel27@users.noreply.github.com>
2021-06-20 14:59:15 -0300

    Update Runge-Kutta-Friction.jl

--- Runge-Kutta-Friction.jl
+++ Runge-Kutta-Friction.jl
@@ -5,16 +5,16 @@ function rungekutta4fri(t0, tf, y0, m₁, m₂, μ₁, μ₂, g, f; N=100)
     w = zeros(m, N+1)
     w[:,1] = y0
     for k = 1:N
-        if (w[2,k] - w[1,k] < 0) 
+        if (w[2,k] - w[1,k] <= 0) 
             w[3,k], w[4,k] = ((m₁ - m₂)*w[3,k] + 2*m₂*w[4,k])/(m₁ + m₂), ((m₂ - m₁)*w[4,k] + 2*m₁*w[3,k])/(m₁ + m₂)
         end
-        if (w[1,k] < 0)
+        if (w[1,k] <= 0)
             w[3,k] = -w[3,k]
         end
-        if (abs(-(k₁/m₁)*(w[1,k] - r₁) + (k₂/m₁)*(w[2,k] - w[1,k] - w₁ - r₂)) < μ₁*g) && ((w[3,k]) == 0.0)
+        if (abs(-(k₁/m₁)*(w[1,k] - r₁) + (k₂/m₁)*(w[2,k] - w[1,k] - w₁ - r₂)) <= μ₁*g) && ((w[3,k]) == 0.0)
             w[3,k] = 0.0
         end
-        if (abs(-(k₂/m₂)*(w[2,k] - w[1,k] - w₁ - r₂)) < μ₂*g) && ((w[4,k]) == 0.0)
+        if (abs(-(k₂/m₂)*(w[2,k] - w[1,k] - w₁ - r₂)) <= μ₂*g) && ((w[4,k]) == 0.0)
             w[4,k] = 0.0
         end
         θ₁ = f(t[k],w[:,k])
